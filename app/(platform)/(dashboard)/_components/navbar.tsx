79f80f26f93ecf819d944f095371aa2d81ee6b9c
Heber Pereira <isaiahpereira88@gmail.com>
2023-11-18 19:47:38 -0800

    create mobile sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -4,10 +4,14 @@ import { Plus } from "lucide-react";
 import { Logo } from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 
+import { MobileSidebar } from "./mobile-sidebar";
+
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 px-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/*TODO:  Mobile Sidebar */}
+      <MobileSidebar />
+
+      {/* DESKTOP NAVBAR */}
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
